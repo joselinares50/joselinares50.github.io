a1442eb0402b9e9514b6c9852cd84a1358eead30
Linares, Jose <joselinares50@gmail.com>
2025-03-22 18:10:16 -0400

    Add dates to timeline.

--- src/app/pages/uikit/timeline.ts
+++ src/app/pages/uikit/timeline.ts
@@ -24,7 +24,7 @@ import { ButtonModule } from 'primeng/button';
                             <p>
                                 {{event.message}}
                             </p>
-                            <p-button label="Read more" [text]="true" />
+                            <!-- <p-button label="Read more" [text]="true" /> -->
                         </p-card>
                     </ng-template>
                 </p-timeline>
@@ -41,89 +41,145 @@ export class Timeline {
     ngOnInit() {
         this.events1 = [
             {
+                status: 'Course Start',
+                date: '01/15/2025',
+                icon: 'pi pi-check',
+                color: 'green',
+                message: ''
+            },
+            {
                 status: 'Module 1: Assigned Reading',
-                date: '15/10/2020 10:30',
+                date: '01/21/2025',
                 icon: 'pi pi-check',
                 color: 'green',
                 message: ''
             },
             {
                 status: 'Module 1: Assignment',
-                date: '15/10/2020 10:30',
+                date: '01/21/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
                 status: 'Module 1: Quiz',
-                date: '15/10/2020 10:30',
+                date: '01/28/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
                 status: 'Module 2: Assigned Reading',
-                date: '15/10/2020 10:30',
+                date: '02/05/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
                 status: 'Module 2: Assignment',
-                date: '15/10/2020 10:30',
+                date: '02/05/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
-                status: 'Module 2: Discussion ',
-                date: '15/10/2020 10:30',
+                status: 'Module 2: Discussion',
+                date: '02/05/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
                 status: 'Module 3: Assigned Reading',
-                date: '15/10/2020 10:30',
+                date: '02/12/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
                 status: 'Module 3: Module',
-                date: '15/10/2020 10:30',
+                date: '02/12/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
                 status: 'Module 3: Quiz',
-                date: '15/10/2020 10:30',
+                date: '02/19/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
                 status: 'Module 4: Assigned Reading',
-                date: '15/10/2020 10:30',
+                date: '02/25/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
                 status: 'Module 4: Assignment',
-                date: '15/10/2020 10:30',
+                date: '02/25/2025',
                 icon: 'pi pi-check',
                 color: 'green'
             },
             {
                 status: 'Module 4: Quiz',
-                date: '15/10/2020 14:00',
+                date: '02/27/2025',
                 icon: 'pi pi-spin pi-spinner',
                 color: '#673AB7'
             },
             {
-                status: 'Module 4: Assigned Reading',
-                date: '15/10/2020 16:15',
+                status: 'Module 5: Assigned Reading',
+                date: '03/05/2025',
                 icon: 'pi pi-minus',
                 color: '#607D8B'
             },
             {
-                status: 'Module 5: Assignment',
-                date: '16/10/2020 10:00',
+                status: 'Module 5: Discussion',
+                date: '03/12/2025',
                 icon: 'pi pi-minus',
                 color: '#607D8B'
+            },
+            {
+                status: 'Module 6: Assignment',
+                date: '03/14/2025',
+                icon: 'pi pi-minus',
+                color: '#607D8B'
+            },
+            {
+                status: 'Module 6: Quiz',
+                date: '03/18/2025',
+                icon: 'pi pi-minus',
+                color: '#607D8B'
+            },
+            {
+                status: 'Mid-Term',
+                date: '03/25/2025',
+                icon: 'pi pi-minus',
+                color: '#607D8B'
+            },
+            {
+                status: 'Module 7: Discussion',
+                date: '03/27/2025',
+                icon: 'pi pi-minus',
+                color: '#607D8B'
+            },
+            {
+                status: 'Module 7: Assignment',
+                date: '03/27/2025',
+                icon: 'pi pi-minus',
+                color: '#607D8B'
+            },
+            {
+                status: 'Module 7: Quiz',
+                date: '04/01/2025',
+                icon: 'pi pi-minus',
+                color: '#607D8B'
+            },
+            {
+                status: 'Final',
+                date: '04/10/2025',
+                icon: 'pi pi-minus',
+                color: '#607D8B'
+            },
+            {
+                status: 'Course End',
+                date: '04/15/2025',
+                icon: 'pi pi-minus',
+                color: '#607D8B',
+                message: ''
             }
         ];
 
