749ba6f4bbcdae8bfaee70d9c6d6d1c01fe4c007
Linares, Jose <joselinares50@gmail.com>
2025-04-21 00:15:09 -0400

    Apply table changes

--- src/app/pages/uikit/grade.ts
+++ src/app/pages/uikit/grade.ts
@@ -68,7 +68,7 @@ interface expandedRows {
                 </ng-template>
             </p-breadcrumb>
             <div class="font-semibold text-xl mb-4 mt-8">Grades</div>
-            <p-table [value]="grouping" sortField="category" sortMode="single" [scrollable]="true" scrollHeight="1000px" rowGroupMode="subheader" groupRowsBy="category" [tableStyle]="{ 'min-width': '60rem' , 'min-height': '30rem'}">
+            <p-table [value]="grouping" showGridlines sortField="category" sortMode="single" [scrollable]="true" scrollHeight="1000px" rowGroupMode="subheader" groupRowsBy="category" [tableStyle]="{ 'min-width': '60rem' , 'min-height': '30rem'}">
                 <ng-template #header>
                     <tr>
                         <th>Category</th>
